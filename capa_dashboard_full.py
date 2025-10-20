d5b99296d7e29d99c2d918250383c822e407a641
noeliabarrios321 <noelia.barrios@sonova.com>
2025-10-20 12:46:05 +0200

    Update capa_dashboard_full.py

--- capa_dashboard_full.py
+++ capa_dashboard_full.py
@@ -1,7 +1,8 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from datetime import datetime
+from datetime import datetime, date
+
 # ==========================
 # CONFIG
 # ==========================
